ae953dc17d8d8c4e98503e949a96ddd55bbeb813
Scheisse <jjwazoo@gmail.com>
2020-05-25 23:38:18 -0700

    Config variables

---
+++ tenma_config.py
@@ -0,0 +1,17 @@
+#########################
+####### Variables #######
+#########################
+
+activities = ["mobile games", "HOW", "POGGER", "$db toddlercon", "with little girls", "pee.com","keta is a pedophile", "rijihuudu.ahlamontada.com"]
+
+db_stream_delay = 5
+
+muted_users_fp = "C:\\Users\\Jace\\Desktop\\Code\\TenmaBot\\muted.txt"
+
+#########################
+#### Commands On/Off ####
+#########################
+config = {
+'bpm':False,
+'stream':False
+}
