e6e22d19e20d40d74dd3b9b73710c91784fad5d3
Scheisse <jjwazoo@gmail.com>
2022-01-27 16:58:39 -0500

    clean config

--- tenma_config.py
+++ tenma_config.py
@@ -2,16 +2,10 @@
 ####### Variables #######
 #########################
 
-activities = ["mobile games", "HOW", "POGGER", "$db toddlercon", "with little girls", "pee.com","keta is a pedophile", "rijihuudu.ahlamontada.com"]
+activities = ["mobile games", "HOW", "POGGER", "pee.com", "rijihuudu.ahlamontada.com", "osu!", "Zach Hill"]
 
 db_stream_delay = 5
-
-muted_users_fp = "C:\\Users\\Jace\\Desktop\\Code\\TenmaBot\\muted.txt"
 
 #########################
 #### Commands On/Off ####
 #########################
-config = {
-'bpm':False,
-'stream':False
-}
